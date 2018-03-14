ac032613f24a5b3c1eef551fd07753b591f14a8f
Menard Gardose <mjgardose@gmail.com>
2018-03-14 15:46:49 +0800

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyCefWrc7ONZWtuh9ydMYyYY-UxsTRFNYw0",
+    authDomain: "fir-finalexam.firebaseapp.com",
+    databaseURL: "https://fir-finalexam.firebaseio.com",
+    projectId: "fir-finalexam",
+    storageBucket: "",
+    messagingSenderId: "568995372062"
+
+
     }
 };
